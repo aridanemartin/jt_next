c5100b11b9f0696519e9d3626391940987f7bab9
aridane-secretsource <aridane@secret-source.eu>
2022-04-16 17:43:09 +0100

    switch head and profile photos depending on innerWidth

--- components/IntroBio/IntroBio.js
+++ components/IntroBio/IntroBio.js
@@ -1,9 +1,9 @@
 import styles from './IntroBio.module.css';
 
-import FotoPerfil from '../../public/images/portada4.jpg';
+import fotoDesktop from '../../public/images/portadaIndex.webp';
+import fotoMobile from '../../public/images/portadaIndex2.jpg'
 import Image from 'next/image';
 import LinkButton from '@components/LinkButton/LinkButton';
-import logoImages from '../../public/images/logos/logos';
 
 import logo1 from "../../public/images/logos/easdLogo.png";
 import logo2 from "../../public/images/logos/getneLogo.png";
@@ -16,19 +16,30 @@ import logo8 from "../../public/images/logos/universidadAlcalaLogo.jpg";
 import logo9 from "../../public/images/logos/universidadAntioquia.jpg";
 import logo10 from "../../public/images/logos/universidadDeCordoba.png";
 
-
+import { useWindowSize } from 'hooks/useWindowDimensions';
+import { useEffect } from 'react';
 
 export default function IntroBio() {
 
+    const size = useWindowSize();
+   
     return (
     <div className={styles.Bio} id="bio">
         <div className={styles.Bio__image} >
+        {size.width > 1100 ?
+                <Image 
+           src={fotoDesktop} 
+           alt="Julian Tamayo || Endocrino en Las Palmas"
+           layout="fill"
+           objectFit="cover"   
+           /> 
+            : 
             <Image 
-            src={FotoPerfil} 
-            alt="Julian Tamayo || Endocrino en Las Palmas"
-            layout="fill"
-            objectFit="cover"   
-            />
+           src={fotoMobile}
+           alt="Julian Tamayo || Endocrino en Las Palmas"
+           layout="fill"
+           objectFit="cover"   
+           />}   
         </div>
         <h2 className={styles.Bio__title}><strong>Doctor Julián Tamayo</strong> <br/> Endocrinología y Nutrición</h2>
         <p className={styles.Bio__text}>Endocrino con más de 15 años de experiencia como profesional en el ejercicio de la medicina. Creo en un enfoque integrado de las emociones y las enfermedades crónicas, por lo que hago parte de un equipo de trabajo en pro de la recuperación de la salud y los buenos hábitos de mis pacientes.<br/><br/> Me apasiona la busqueda individual de los objetivos de la mano del paciente, de su familia y de su entorno.
