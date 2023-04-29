ae4c19be77f35b12301c76176036c331c8d804c1
Aridane Martín <aridanemartin@gmail.com>
2023-04-29 12:41:36 +0100

    New blog features

--- components/SanityBlock/SanityBlock.js
+++ components/SanityBlock/SanityBlock.js
@@ -5,19 +5,48 @@ import sanityClient from "../../client";
 import styles from "./SanityBlock.module.scss";
 import Link from "next/link";
 
-function SanityBlock({ sanityContent }) {
+import { getYoutubeVideoId } from "helpers/youtubeHelpers";
+
+function SanityBlock({ sanityContent, key }) {
   const builder = imageUrlBuilder(sanityClient);
   function imageUrlFor(source) {
     return builder.image(source);
   }
 
-  const formatAndRenderText = (child) => {
+  const headerStyles = {
+    h1: ({ children }) => (
+      <h1 className={`${styles.h1} ${styles.title}`}>{children}</h1>
+    ),
+    h2: ({ children }) => (
+      <h2 className={`${styles.h2} ${styles.title}`}>{children}</h2>
+    ),
+    h3: ({ children }) => (
+      <h3 className={`${styles.h3} ${styles.title}`}>{children}</h3>
+    ),
+    h4: ({ children }) => (
+      <h4 className={`${styles.h4} ${styles.title}`}>{children}</h4>
+    ),
+    h5: ({ children }) => (
+      <h5 className={`${styles.h5} ${styles.title}`}>{children}</h5>
+    ),
+    h6: ({ children }) => (
+      <h6 className={`${styles.h6} ${styles.title}`}>{children}</h6>
+    ),
+  };
+
+  const formatAndRenderText = (child, style) => {
     const enrichedLinksKeys = sanityContent.markDefs
       .filter((mark) => mark._type === "link")
       .map((mark) => mark._key);
 
     if (child.text === "") {
       return <br key={child._key} />;
+    }
+
+    // Titles [h1, h2, h3, h4, h5, h6]
+    if (headerStyles[style]) {
+      const Header = headerStyles[style];
+      return <Header>{child.text}</Header>;
     }
 
     if (child.marks.some((mark) => enrichedLinksKeys.includes(mark))) {
@@ -27,6 +56,7 @@ function SanityBlock({ sanityContent }) {
 
       return (
         <Link
+          className={`${styles.paragraph} ${styles.link}`}
           href={`https://${link.href}`}
           target="_blank"
           rel="noReferrer"
@@ -40,7 +70,7 @@ function SanityBlock({ sanityContent }) {
     if (child.marks) {
       if (child.marks.includes("strong") && child.marks.includes("em")) {
         return (
-          <span key={child._key}>
+          <span className={styles.paragraph} key={child._key}>
             <strong>
               <em>{child.text}</em>
             </strong>
@@ -50,21 +80,21 @@ function SanityBlock({ sanityContent }) {
 
       if (child.marks.includes("strong")) {
         return (
-          <span key={child._key}>
+          <span className={styles.paragraph} key={child._key}>
             <strong>{child.text}</strong>
           </span>
         );
       }
       if (child.marks.includes("em")) {
         return (
-          <span key={child._key}>
+          <span className={styles.paragraph} key={child._key}>
             <em>{child.text}</em>
           </span>
         );
       }
     }
 
-    return <span>{child.text}</span>;
+    return <span className={styles.paragraph}>{child.text}</span>;
   };
 
   const renderBlock = (sanityContent) => {
@@ -72,13 +102,15 @@ function SanityBlock({ sanityContent }) {
       case "block":
         return (
           <div className={styles.block}>
-            {sanityContent.children.map((child) => formatAndRenderText(child))}
+            {sanityContent.children.map((child) =>
+              formatAndRenderText(child, sanityContent.style)
+            )}
           </div>
         );
 
       case "enrichedImage":
         return (
-          <div className={styles.imageBlock}>
+          <div className={styles.imageBlockHorizontal}>
             <Image
               src={imageUrlFor(sanityContent.image.asset).url()}
               fill
@@ -87,6 +119,32 @@ function SanityBlock({ sanityContent }) {
             />
           </div>
         );
+      case "enrichedImageVertical":
+        return (
+          <div className={styles.imageBlockVertical}>
+            <Image
+              src={imageUrlFor(sanityContent.image.asset).url()}
+              fill
+              style={{ objectFit: "contain" }}
+              alt={sanityContent.alt}
+            />
+          </div>
+        );
+      case "youtubeVideo":
+        return (
+          <div className={styles.videoBlock}>
+            <iframe
+              src={`https://www.youtube.com/embed/${getYoutubeVideoId(
+                sanityContent.url
+              )}`}
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen={true}
+              title={sanityContent.title}
+            ></iframe>
+          </div>
+        );
+
       default:
         return null;
     }
--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,11 +1,10 @@
-import BlockContent from "@sanity/block-content-to-react";
 import Link from "next/link";
 import sanityClient from "../../client";
 
 import Image from "next/image";
 import { useNextSanityImage } from "next-sanity-image";
 
-import styles from "@styles/SinglePost.module.css";
+import styles from "@styles/SinglePost.module.scss";
 import Meta from "@components/Meta/Meta";
 import SanityBlock from "@components/SanityBlock/SanityBlock";
 import Layout from "@components/Layout/Layout";
@@ -13,7 +12,6 @@ import Layout from "@components/Layout/Layout";
 export const SinglePost = (data) => {
   const mainImageProps = useNextSanityImage(sanityClient, data.mainImage);
   const authorImageProps = useNextSanityImage(sanityClient, data.author.image);
-
   return (
     <>
       <Meta
@@ -24,7 +22,6 @@ export const SinglePost = (data) => {
       />
       <div>
         <div className={styles.postImageHero}>
-          <h2 className={styles.title}>{data.title}</h2>
           <Image
             src={mainImageProps?.src}
             className={styles.imageHero}
@@ -37,30 +34,40 @@ export const SinglePost = (data) => {
           />
         </div>
       </div>
+      <Layout></Layout>
       <article className={styles.contentWrapper}>
-        <Link href={`/equipo/${data.author.slug.current}#conoceme`}>
-          <div className={styles.authorData}>
-            <div className={styles.dataWrap}>
-              <p className={styles.date}>{data.author.especialidad}</p>
-              <p className={styles.author}>{data.author.name}</p>
-              <div className={styles.authorImage}>
-                <Image
-                  src={authorImageProps?.src}
-                  alt={`${data.author.name} Foto de Perfil`}
-                  fill
-                  quality={25}
-                  sizes="100vw"
-                  style={{
-                    objectFit: "cover",
-                  }}
-                />
+        <Layout small>
+          <h2 className={styles.title}>{data.title}</h2>
+          <div className={styles.subtitle}>
+            <Link href={`/equipo/${data.author.slug.current}#conoceme`}>
+              <div className={styles.author}>
+                <div className={styles.author__text}>
+                  <p className={styles.name}>{data.author.name}</p>
+                  <p className={styles.specialty}>{data.author.especialidad}</p>
+                </div>
+                <div className={styles.author__image}>
+                  <Image
+                    src={authorImageProps?.src}
+                    alt={`${data.author.name} Foto de Perfil`}
+                    fill
+                    quality={25}
+                    sizes="100vw"
+                    style={{
+                      objectFit: "cover",
+                    }}
+                  />
+                </div>
               </div>
+            </Link>
+            <div className={styles.timeToRead}>
+              <p className={styles.time}>| {data.timeToRead} min de lectura</p>
             </div>
           </div>
-        </Link>
-        <Layout small>
           {data.body.map((block) => (
-            <SanityBlock key={block._key} sanityContent={block} />
+            <>
+              {console.log(block)}
+              <SanityBlock key={block._key} sanityContent={block} />
+            </>
           ))}
         </Layout>
       </article>
@@ -85,11 +92,7 @@ export const getServerSideProps = async (pageContext) => {
         body,
         mainImage,
         description,
-        body1,
-        articleImage1,
-        body2,
-        articleImage2,
-        body3,
+        timeToRead,
         author->
     }`);
   const url = `https://6yfev950.api.sanity.io/v2021-08-31/data/query/production?query=${query}`;
@@ -101,15 +104,11 @@ export const getServerSideProps = async (pageContext) => {
     props: {
       data: post,
       slug: pageSlug,
+      timeToRead: post.timeToRead,
       body: post.body,
-      body3: post.body3,
       title: post.title,
       mainImage: post.mainImage,
       description: post.description,
-      body1: post.body1,
-      image1: post.articleImage1,
-      body2: post.body2,
-      image2: post.articleImage2,
       author: post.author,
     },
   };
